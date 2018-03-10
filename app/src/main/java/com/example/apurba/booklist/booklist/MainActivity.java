6eceba1bc73831af9092e2d3b9266e39a79508c7
Apurba Biswas <apurba10024@gmail.com>
2018-03-10 15:47:40 +0600

    1.7 Added empty state messege and internet connection detection

--- app/src/main/java/com/example/apurba/booklist/booklist/MainActivity.java
+++ app/src/main/java/com/example/apurba/booklist/booklist/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.apurba.booklist.booklist;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private String googleBookRequestUrl = "https://www.googleapis.com/books/v1/volumes?q=";
     private booksAdapater mAdapter;
     private TextView textView;
+    private TextView mEmptyStateTextView;
+    private View loading_indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +37,42 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new booksAdapater(this, books);
         booksListView.setAdapter(mAdapter);
 
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        booksListView.setEmptyView(mEmptyStateTextView);
+
+        loading_indicator = findViewById(R.id.loading_spinner);
+        loading_indicator.setVisibility(View.GONE);
+
         Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String givenText = editText.getText().toString();
 
-                loadBooks(givenText);
-
+                loading_indicator.setVisibility(View.VISIBLE);
+                if(isConnectedToInternet()){
+                    loadBooks(givenText);
+                }else{
+                    loading_indicator.setVisibility(View.GONE);
+                    mEmptyStateTextView.setText(R.string.no_internet_connection);
+                }
             }
         });
     }
+
+    /**
+     * Detect internet connection
+     */
+    private boolean isConnectedToInternet(){
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        return isConnected;
+    }
+
     /* when user clicks the search button to search this method
-     * gets triggered and laod books using asynck task
+     * gets triggered and laod books using async task
      */
     private void loadBooks(String input){
         if(!input.equals("")){
@@ -53,7 +82,8 @@ public class MainActivity extends AppCompatActivity {
             task.execute(googleBookRequestUrl);
             googleBookRequestUrl = "https://www.googleapis.com/books/v1/volumes?q=";
         }else{
-            textView.setText("Bad input");
+            mEmptyStateTextView.setText(R.string.bad_input);
+            loading_indicator.setVisibility(View.GONE);
         }
     }
 
@@ -73,10 +103,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<BookInfo> bookInfos) {
             mAdapter.clear();
-
+            loading_indicator.setVisibility(View.GONE);
             if (bookInfos != null && !bookInfos.isEmpty()) {
                 mAdapter.addAll(bookInfos);
             }
+            mEmptyStateTextView.setText(R.string.no_books_found);
             googleBookRequestUrl = "https://www.googleapis.com/books/v1/volumes?q=";
         }
     }
